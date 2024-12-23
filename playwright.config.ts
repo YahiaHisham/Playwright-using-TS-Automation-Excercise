da7e75a6c5a8568a601d271bd6f77ebe5c869a59
YahiaHisham <yahia.hisham65@gmail.com>
2024-12-23 21:06:30 +0200

    adding allure report and retry failed cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,20 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
+    timeout: 6*1000,
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    /* Retry Failed Cases */
+    retries: 1,
+    /* how many workers in run paralell */
+    workers: undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [
+        ['html'],
+        ['allure-playwright']
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -29,7 +33,11 @@ export default defineConfig({
             use: {
                 ...devices['Desktop Chrome'],
                 baseURL: 'https://automationexercise.com',
-                headless: true
+                headless: true,
+                screenshot: 'on',
+                video: 'retain-on-failure',
+                trace: 'on',
             },
-        }]
+        }
+    ]
 });
--- tests/testClasses/ContactUsTest.spec.ts
+++ tests/testClasses/ContactUsTest.spec.ts
@@ -4,15 +4,23 @@ import { SignupLoginPage } from '../../pages/SignupLoginPage';
 import { faker } from '@faker-js/faker'
 import JsonReader from '../utilities/JsonReader'
 import { ContactUsPage } from '../../pages/ContactUsPage';
+import * as allure from "allure-js-commons";
 
 test.beforeEach('has title', async ({ page }) => {
     // this is to read te base URL from playwright.config file
     await page.goto('/');
 });
 
-test('verify that user cant login using invalid credentials @smoke @regression', async ({ page }) => {
+test('verify that user cant login using invalid credentials @regression', async ({ page }) => {
+    await allure.displayName("Contact Us Using Email");
+    await allure.owner("Yahia Hisham");
+    await allure.severity("critical");
+    await allure.epic("Web");
+    await allure.feature("Contact Us");
+    await allure.story("As User, I want to send an email including my complaint");
+    await allure.suite("Regression Test");
     await new HomePage(page).clickOnContactUsButton();
-    await expect(page.getByRole('heading', { name: 'Get In Touch' })).toHaveText('Get In Touch');
+    await expect(page.getByRole('heading', { name: 'Get In Touch' })).toHaveText('Get In Toouch');
     await new ContactUsPage(page).uploadFile();
     await new ContactUsPage(page).fillFormData('Yahia', 'yahia@mainModule.coom', 'subject example', 'long long long message');
     page.on('dialog', dialog => {
